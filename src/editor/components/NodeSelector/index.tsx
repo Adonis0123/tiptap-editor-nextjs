0c22b2ea8cf7e05a0480facfa5f97230d06c3985
Adonis0123 <513554676@qq.com>
2023-06-26 13:53:43 +0800

    feat: 新增流式展示

--- src/editor/components/NodeSelector/index.tsx
+++ src/editor/components/NodeSelector/index.tsx
@@ -1,7 +1,7 @@
-import { Dispatch, FC, SetStateAction } from "react";
-import { Editor } from "@tiptap/core";
-import { BubbleMenuItem } from "../EditorBubbleMenu";
-import cx from "classnames";
+import { Dispatch, FC, SetStateAction } from 'react';
+import { Editor } from '@tiptap/core';
+import { BubbleMenuItem } from '../EditorBubbleMenu';
+import cx from 'classnames';
 import {
   Check,
   ChevronDown,
@@ -13,7 +13,7 @@ import {
   TextIcon,
   Code,
   CheckSquare,
-} from "lucide-react";
+} from 'lucide-react';
 
 interface NodeSelectorProps {
   editor: Editor;
@@ -28,74 +28,73 @@ export const NodeSelector: FC<NodeSelectorProps> = ({
 }) => {
   const items: BubbleMenuItem[] = [
     {
-      name: "Text",
+      name: 'Text',
       icon: TextIcon,
       command: () =>
-        editor.chain().focus().toggleNode("paragraph", "paragraph").run(),
-      // I feel like there has to be a more efficient way to do this – feel free to PR if you know how!
+        editor.chain().focus().toggleNode('paragraph', 'paragraph').run(),
       isActive: () =>
-        editor.isActive("paragraph") &&
-        !editor.isActive("bulletList") &&
-        !editor.isActive("orderedList"),
+        editor.isActive('paragraph') &&
+        !editor.isActive('bulletList') &&
+        !editor.isActive('orderedList'),
     },
     {
-      name: "Heading 1",
+      name: 'Heading 1',
       icon: Heading1,
       command: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
-      isActive: () => editor.isActive("heading", { level: 1 }),
+      isActive: () => editor.isActive('heading', { level: 1 }),
     },
     {
-      name: "Heading 2",
+      name: 'Heading 2',
       icon: Heading2,
       command: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
-      isActive: () => editor.isActive("heading", { level: 2 }),
+      isActive: () => editor.isActive('heading', { level: 2 }),
     },
     {
-      name: "Heading 3",
+      name: 'Heading 3',
       icon: Heading3,
       command: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
-      isActive: () => editor.isActive("heading", { level: 3 }),
+      isActive: () => editor.isActive('heading', { level: 3 }),
     },
     {
-      name: "To-do List",
+      name: 'To-do List',
       icon: CheckSquare,
       command: () => editor.chain().focus().toggleTaskList().run(),
-      isActive: () => editor.isActive("taskItem"),
+      isActive: () => editor.isActive('taskItem'),
     },
     {
-      name: "Bullet List",
+      name: 'Bullet List',
       icon: ListOrdered,
       command: () => editor.chain().focus().toggleBulletList().run(),
-      isActive: () => editor.isActive("bulletList"),
+      isActive: () => editor.isActive('bulletList'),
     },
     {
-      name: "Numbered List",
+      name: 'Numbered List',
       icon: ListOrdered,
       command: () => editor.chain().focus().toggleOrderedList().run(),
-      isActive: () => editor.isActive("orderedList"),
+      isActive: () => editor.isActive('orderedList'),
     },
     {
-      name: "Quote",
+      name: 'Quote',
       icon: TextQuote,
       command: () =>
         editor
           .chain()
           .focus()
-          .toggleNode("paragraph", "paragraph")
+          .toggleNode('paragraph', 'paragraph')
           .toggleBlockquote()
           .run(),
-      isActive: () => editor.isActive("blockquote"),
+      isActive: () => editor.isActive('blockquote'),
     },
     {
-      name: "Code",
+      name: 'Code',
       icon: Code,
       command: () => editor.chain().focus().toggleCodeBlock().run(),
-      isActive: () => editor.isActive("codeBlock"),
+      isActive: () => editor.isActive('codeBlock'),
     },
   ];
 
   const activeItem = items.filter((item) => item.isActive()).pop() ?? {
-    name: "Multiple",
+    name: 'Multiple',
   };
 
   return (
@@ -119,9 +118,9 @@ export const NodeSelector: FC<NodeSelectorProps> = ({
                 setIsOpen(false);
               }}
               className={cx(
-                "flex items-center justify-between rounded-sm px-2 py-1 text-sm text-stone-600 hover:bg-stone-100",
+                'flex items-center justify-between rounded-sm px-2 py-1 text-sm text-stone-600 hover:bg-stone-100',
                 {
-                  "text-blue-600": item.isActive(),
+                  'text-blue-600': item.isActive(),
                 }
               )}
             >
--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -1,25 +1,52 @@
-import React, { useEffect, useState } from "react";
-import { presetMarkdown } from "./default-content";
-import { EditorContent, useEditor } from "@tiptap/react";
-import { TiptapExtensions } from "./extensions";
-import { TiptapEditorProps } from "./props";
-import { markdown2Html } from "./utils";
-import { EditorBubbleMenu } from "./components/EditorBubbleMenu";
-import ToolsBar from "./components/ToolsBar";
+import React, { useEffect, useState } from 'react';
+import { presetMarkdown } from './default-content';
+import { EditorContent, useEditor } from '@tiptap/react';
+import { TiptapExtensions } from './extensions';
+import { TiptapEditorProps } from './props';
+import { markdown2Html } from './utils';
+import { EditorBubbleMenu } from './components/EditorBubbleMenu';
+import ToolsBar from './components/ToolsBar';
 
 import classNames from 'classnames';
 export interface IEditorProps {}
 
 const Editor: React.FC<IEditorProps> = (props) => {
-  const [markdown, setMarkdown] = useState(presetMarkdown);
+  const [markdown, setMarkdown] = useState('');
+
   /* 初始化editor */
   const editor = useEditor({
     extensions: TiptapExtensions,
     editorProps: TiptapEditorProps,
-    content: "",
-    autofocus: "end",
+    content: '',
+    autofocus: 'end',
     onUpdate: (e) => {},
   });
+
+  useEffect(() => {
+    if (editor) {
+      editor.setEditable(false);
+      // 获取浏览器参数
+      const url = new URL(window.location.href);
+      const searchParams = url.searchParams;
+      const init = searchParams.get('init');
+
+      if (init === 'stream') {
+        let i = 0;
+        const interval = setInterval(() => {
+          setMarkdown(presetMarkdown.substring(0, i));
+          i++;
+          if (i > presetMarkdown.length) {
+            clearInterval(interval);
+            editor.setEditable(true);
+          }
+        }, 30);
+        return () => clearInterval(interval);
+      } else {
+        setMarkdown(presetMarkdown);
+        editor.setEditable(true);
+      }
+    }
+  }, [editor]);
 
   useEffect(() => {
     if (editor && markdown) {
@@ -35,9 +62,11 @@ const Editor: React.FC<IEditorProps> = (props) => {
           <ToolsBar editor={editor} />
           <div
             style={{
-              boxShadow: "0px 4px 9px rgba(11, 45, 96, 0.16)",
+              boxShadow: '0px 4px 9px rgba(11, 45, 96, 0.16)',
             }}
-            className={classNames(`w-full h-full bg-white rounded-lg p-6 pl-8 overflow-y-auto tiptap-wrap`)}
+            className={classNames(
+              `w-full h-full bg-white rounded-lg p-6 pl-8 overflow-y-auto tiptap-wrap`
+            )}
           >
             <div className="relative">
               <EditorContent className={` `} editor={editor} />
